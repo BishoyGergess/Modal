095bd3d4f5bde30380f13a3fd94b9a6251780d52
BishoyMilad <bishoyanwar@outlook.com>
2022-01-21 16:21:35 -0500

    Open & close modal added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const overlayModal = document.querySelector('.overlay');
+const showModals = document.querySelectorAll('.show-modal');
+
+const clickCloseModal = function () {
+  modal.classList.add('hidden');
+  overlayModal.classList.add('hidden');
+};
+const clickOpenModal = function () {
+  modal.classList.remove('hidden');
+  overlayModal.classList.remove('hidden');
+};
+for (let i = 0; i < showModals.length; i++)
+  showModals[i].addEventListener('click', clickOpenModal);
+
+closeModal.addEventListener('click', clickCloseModal);
+overlayModal.addEventListener('click', clickCloseModal);
